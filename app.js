fa0203612cde80f36e7bdc5f097907b51a54b4f9
jfilinho <filinhosouza10@gmail.com>
2022-04-06 19:43:40 -0300

    commit urls

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 // Não esquecer de criar variável de ambiente com o endereço do seu app React (local ou deployado no Netlify)
-app.use(cors({ origin: process.env.REACT_APP_URL }));
 
 const usuarioRouter = require("./routes/user.routes");
 app.use("/api", usuarioRouter);
@@ -17,3 +16,4 @@ app.use("/api", usuarioRouter);
 app.listen(Number(process.env.PORT), () =>
   console.log(`Server up and running at port ${process.env.PORT}`)
 );
+app.use(cors({ origin: process.env.REACT_APP_URL }));
